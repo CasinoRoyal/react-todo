854865841a82d92bc77c55036fb525877e678648
CasinoRoyal <lux_ex_tenebris@outlook.com>
2019-06-09 16:47:49 +0300

    Create Todo

---
+++ src/components/app-header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AppHeader = () => {
+  return <input placeholder="Search"/>
+};
+
+export default AppHeader;
---
+++ src/components/todo-list-item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoItem = () => {
+  return <span> Drink Coffee </span>
+};
+
+export default TodoItem;
